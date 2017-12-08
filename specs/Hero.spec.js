9508f1a3746c6946633696396b4689b9387a1fee
Chris Marshall <cmarshall10450@gmail.com>
2017-12-08 10:32:45 +0000

    Hero should have a name

--- specs/Hero.spec.js
+++ specs/Hero.spec.js
@@ -11,4 +11,12 @@ describe('Hero', function () {
   it('should have a name', function () {
     assert(hero.name, 'Superman')
   })
+
+  it('should have a health value', function () {
+    assert(hero.health, 100)
+  })
+
+  it('should have a favourite food', function () {
+    assert(hero.favouriteFoor, 'Mac & Cheese')
+  })
 })
